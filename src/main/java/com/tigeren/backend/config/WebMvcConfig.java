4e2c1aad12933eb9b5510b986de7ccf8933b9fa9
QuanT4_HoangMai <davidnguyen67dev@gmail.com>
2025-04-06 21:40:13 +0700

    fix (CORS): : Refactor backend configuration: remove SpringConfig, add WebMvcConfig with CORS support and ModelMapper bean; enhance BackendApplication with Lombok logging

---
+++ src/main/java/com/tigeren/backend/config/WebMvcConfig.java
@@ -0,0 +1,28 @@
+package com.tigeren.backend.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Slf4j
+@Configuration
+@EnableScheduling
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Bean
+    public ModelMapper modelMapperBean() {
+        return new ModelMapper();
+    }
+
+    @Override
+    public void addCorsMappings(final CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
